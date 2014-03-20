4b00454fd1a3d514d3147f3132cc2d1677eefe48
Coleman Foley <colemanfoley@gmail.com>
2014-03-20 11:14:13 -0700

    Now sets created attribute of charges when they are created, which indicates the time when the charge is created. Removed the created field from the charge-creation form, and made created a protected attribute that can't be set by the user.

--- source/store/app/models/charge.rb
+++ source/store/app/models/charge.rb
@@ -1,3 +1,10 @@
 class Charge < ActiveRecord::Base
 	belongs_to :customer
+	attr_accessible :paid, :amount, :currency, :refunded, :customer_id
+	before_create :set_created_property
+
+	private
+	def set_created_property
+		self.created = DateTime.now
+	end
 end
