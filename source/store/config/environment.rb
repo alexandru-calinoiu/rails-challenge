e733192d17271a927825994f70765b63a1d93474
Coleman Foley <colemanfoley@gmail.com>
2014-03-21 17:18:58 -0700

    Explicitly required ejs, in response to a warning received in running tests.

--- source/store/config/environment.rb
+++ source/store/config/environment.rb
@@ -1,5 +1,8 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
+# I required this because running the rspec test on the charges index view gave a warning, which recommended explicitly requiring EJS.
+require 'ejs'
+
 # Initialize the Rails application.
 Store::Application.initialize!
